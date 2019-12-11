af1793722b5be9169d5f1c5f7c0f0df58c0824be
wukaiying <wukaiying494@pingan.com.cn>
2019-12-11 18:33:23 +0800

    add mango db

--- consignment-service/handler.go
+++ consignment-service/handler.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"context"
+	"log"
+	pb "shippy/consignment-service/proto/consignment"
+	vesselpb "shippy/vessel-service/proto/vessel"
+
+	"gopkg.in/mgo.v2"
+)
+
+// 微服务服务端 struct handler 必须实现 protobuf 中定义的 rpc 方法
+// 实现方法的传参等可参考生成的 consignment.pb.go
+type handler struct {
+	session *mgo.Session
+	vesselClient vesselpb.VesselService
+}
+
+// 从主会话中 Clone() 出新会话处理查询
+func (h *handler)GetRepo() Repository  {
+	return &ConsignmentRepository{h.session.Clone()}
+}
+
+//
+// service 实现 consignment.pb.go 中的 ShippingServiceServer 接口
+// 使 service 作为 gRPC 的服务端
+//
+// 托运新的货物
+func (h *handler) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	defer h.GetRepo().Close()
+
+	//检查是否有合适的货轮
+	vReq := &vesselpb.Specification{
+		Capacity: int32(len(req.Containers)),
+		MaxWeight: req.Weight,
+	}
+	vResp, err := h.vesselClient.FindAvailable(context.Background(), vReq)
+	if err != nil {
+		return err
+	}
+	log.Printf("found vessel: %s\n", vResp.Vessel.Name)
+	req.VesselId = vResp.Vessel.Id
+
+	// 接收承运的货物
+	err = h.GetRepo().Create(req)
+	if err != nil {
+		return err
+	}
+	res.Created = true
+	res.Consignment = req
+	return nil
+}
+
+//查询consignment 服务
+func (h *handler) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	allConsignments, err := h.GetRepo().GetAll()
+	if err != nil {
+		return err
+	}
+	res.Consignments = allConsignments
+	return nil
+}
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -4,8 +4,8 @@
 package main
 
 import (
-    "context"
     "log"
+    "os"
 
     "github.com/micro/go-micro"
     // 导如 protoc 自动生成的包
@@ -17,81 +17,21 @@ const (
     DEFAULT_HOST = "localhost:27017"
 )
 
-//
-// 仓库接口
-//
-type IRepository interface {
-    Create(consignment *pb.Consignment) (*pb.Consignment, error) // 存放新货物
-    GetAll() []*pb.Consignment
-}
-
-//
-// 我们存放多批货物的仓库，实现了 IRepository 接口
-// Repository - 虚拟存储库，模拟数据存储的使用，以后会用一个真正的实现来替换
-//
-type Repository struct {
-    consignments []*pb.Consignment
-}
-
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-    repo.consignments = append(repo.consignments, consignment)
-    return consignment, nil
-}
-
-func (repo *Repository) GetAll() []*pb.Consignment {
-    return repo.consignments
-}
-
-//
-// 定义微服务
-//
-type service struct {
-    repo Repository
-    vesselClient vesselpb.VesselService  //添加vessel client
-}
-
-//
-// service 实现 consignment.pb.go 中的 ShippingServiceServer 接口
-// 使 service 作为 gRPC 的服务端
-//
-// 托运新的货物
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-    //检查是否有合适的货轮
-    vReq := &vesselpb.Specification{
-        Capacity: int32(len(req.Containers)),
-        MaxWeight: req.Weight,
-    }
-    vResp, err := s.vesselClient.FindAvailable(context.Background(), vReq)
-    if err != nil {
-        return err
-    }
-    log.Printf("found vessel: %s\n", vResp.Vessel.Name)
-    req.VesselId = vResp.Vessel.Id
-
-    // 接收承运的货物
-    consignment, err := s.repo.Create(req)
-    if err != nil {
-        return err
-    }
-    //resp := &pb.Response{Created: true, Consignment: consignment}
-
-    res.Created = true
-    res.Consignment = consignment
-    return nil
-}
-
-//查询consignment 服务
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	allConsignments := s.repo.GetAll()
-	//resp := &pb.Response{Consignments: allConsignments}
-	res.Consignments = allConsignments
-    return nil
-}
-
-
 func main() {
-    // 创建一个新服务，其中可以包括一些可选的配置
+    // 获取容器设置的数据库地址环境变量的值
+    dbHost := os.Getenv("DB_HOST")
+    if dbHost == ""{
+        dbHost = DEFAULT_HOST
+    }
+    //创建session，handler中使用clone来使用session.
+    session, err := CreateSession(dbHost)
+    // 创建于 MongoDB 的主会话，需在退出 main() 时候手动释放连接
+    defer session.Close()
+    if err != nil {
+        log.Fatalf("create session error: %v\n", err)
+    }
 
+    // 创建一个新服务，其中可以包括一些可选的配置
     server := micro.NewService(
         // 这个 Name 必须和 consignment.proto 中的 package 一致
 
@@ -99,10 +39,8 @@ func main() {
         micro.Version("latest"),
         )
     server.Init()
-    repo := Repository{}
-
     vesselClient := vesselpb.NewVesselService("go.micro.srv.vessel", server.Client())
-    pb.RegisterShippingServiceHandler(server.Server(),  &service{repo,vesselClient})
+    pb.RegisterShippingServiceHandler(server.Server(),  &handler{session,vesselClient})
     if err := server.Run(); err != nil {
         log.Fatalf("failed to serve: %v", err)
     }
